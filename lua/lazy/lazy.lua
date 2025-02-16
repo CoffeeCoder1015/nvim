bdcb71ef5c56159506332823a9fe55af25a7fb5f
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-02-16 14:05:53 +1300

    switch to catppuccin

--- lua/lazy/lazy.lua
+++ lua/lazy/lazy.lua
@@ -21,26 +21,46 @@ require("lazy").setup({
 			"LazyVim/LazyVim",
 			import = "lazyvim.plugins",
 			opts = {
-				colorscheme = "tokyonight-night",
-				defaults = {},
+				colorscheme = function()
+					require("catppuccin").load()
+				end,
 			},
 		},
-		{ -- You can easily change to a different colorscheme.
-			-- Change the name of the colorscheme plugin below, and then
-			-- change the command in the config to whatever the name of that colorscheme is.
-			--
-			-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-			"folke/tokyonight.nvim",
-			priority = 1000, -- Make sure to load this before all the other start plugins.
-			init = function()
-				-- Load the colorscheme here.
-				-- Like many other themes, this one has different styles, and you could load
-				-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-				vim.cmd.colorscheme("tokyonight-night")
-
-				-- You can configure highlights by doing something like:
-				vim.cmd.hi("Comment gui=none")
-			end,
+		{
+			"catppuccin/nvim",
+			name = "catppuccin",
+			priority = 1000,
+			opts = {
+				no_italic = true,
+				term_colors = true,
+				transparent_background = false,
+				styles = {
+					comments = {},
+					conditionals = {},
+					loops = {},
+					functions = {},
+					keywords = {},
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+				},
+				color_overrides = {
+					mocha = {
+						base = "#010504",
+						mantle = "#111212",
+						crust = "#bed1cb",
+					},
+				},
+				integrations = {
+					dropbar = {
+						enabled = true,
+						color_mode = true,
+					},
+				},
+			},
 		},
 		-- import/override with your plugins
 		{ import = "lazy.plugins" },
