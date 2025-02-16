31745aefe61992d5dc0ab03fa42fcb852d8706ed
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-02-16 14:47:47 +1300

    Made powershell work

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,6 +15,9 @@ vim.opt.expandtab = true
 
 -- Set default shell
 vim.o.shell = "powershell"
+vim.o.shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+vim.o.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+vim.o.shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
