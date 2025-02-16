5b2af8093a735a8af8f860b223acad961faf5d24
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-02-16 13:00:19 +1300

    git category

--- lua/lazy/lazy.lua
+++ lua/lazy/lazy.lua
@@ -30,20 +30,6 @@ require("lazy").setup({
 
 			-- "gc" to comment visual regions/lines
 			{ "numToStr/Comment.nvim", opts = {} },
-
-			{ -- Adds git related signs to the gutter, as well as utilities for managing changes
-				"lewis6991/gitsigns.nvim",
-				opts = {
-					signs = {
-						add = { text = "+" },
-						change = { text = "~" },
-						delete = { text = "_" },
-						topdelete = { text = "‾" },
-						changedelete = { text = "~" },
-					},
-				},
-			},
-
 			{ -- You can easily change to a different colorscheme.
 				-- Change the name of the colorscheme plugin below, and then
 				-- change the command in the config to whatever the name of that colorscheme is.
